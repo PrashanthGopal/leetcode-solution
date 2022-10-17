4ba7926590563fb875b049aa73019ddc51fc7ede
PrashanthGopal <gopal.prashi@gmail.com>
2022-10-17 08:16:49 +0530

    Time: 3 ms (63.16%), Space: 50.7 MB (40.51%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        if(nums == null){
+            return -1;
+        }
+        int result = 0;
+        for(int num : nums){
+            result ^= num;
+        }
+        
+        return result;
+    }
+}
