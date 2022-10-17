ed003371a76af998f74239dfbd4493228fe9da53
PrashanthGopal <gopal.prashi@gmail.com>
2022-10-17 15:14:09 +0530

    Time: 182 ms (41.10%), Space: 137.9 MB (5.55%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -14,19 +14,5 @@ class Solution {
             stack.push(i);
         }
         return result;
-        
-        
-    //     Stack<Integer> stack = new Stack<>();
-    // int[] ret = new int[temperatures.length];
-    // for(int i = 0; i < temperatures.length; i++) {
-    //     while(!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
-    //         int idx = stack.pop();
-    //         ret[idx] = i - idx;
-    //     }
-    //     stack.push(i);
-    // }
-    // return ret;
-        
-        
     }
 }
