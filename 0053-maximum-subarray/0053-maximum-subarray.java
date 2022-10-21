a62cd790ce5dfebd04b46ebd21ac8c8932c8a63e
PrashanthGopal <gopal.prashi@gmail.com>
2022-10-21 21:26:25 +0530

    Time: 1 ms (100.00%), Space: 51.2 MB (99.37%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -3,15 +3,12 @@ class Solution {
         if(nums == null || nums.length == 0){
             return -1;
         }
-        int largest = nums[0];
-        int dp[] = new int[nums.length + 1];
-        dp[0] = nums[0];
+        int max = nums[0], maxCur = nums[0];
         for(int i=1; i<nums.length; i++){
-            dp[i] = Math.max(dp[i-1]+nums[i], nums[i]);
-            if(largest< dp[i]){
-                largest = dp[i];
-            }
+            maxCur = Math.max(maxCur + nums[i], nums[i]);
+            max = Math.max(max, maxCur);
+            
         }
-        return largest;
+        return max;
     }
 }
